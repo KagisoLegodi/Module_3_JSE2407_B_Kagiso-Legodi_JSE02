6109e56453be6c4caa07e1db7e18fa876ef76980
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-07-30 13:24:33 +0200

    "Added nav element with styles to Header component"

--- src/Header.svelte
+++ src/Header.svelte
@@ -9,5 +9,9 @@
   </script>
 
   <header class="sticky top-0 z-50">
-    
+    <nav class="bg-red-500 border-yellow-200">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            
+        </div>
+    </nav>
   </header>
